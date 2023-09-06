8e79e4b500a5f941c9d082300959de18a14e286d
Dmitry Kuznetsov <dmitry.kuznetsov.it@gmail.com>
2023-09-06 19:52:27 +0200

    add waiting component

---
+++ src/components/Spinner.vue
@@ -0,0 +1,51 @@
+<template>
+  <span class="Spinner"></span>
+</template>
+
+<style>
+.Spinner {
+  width: 200px;
+  height: 200px;
+  border: 10px dotted var(--color-secondary);
+  border-style: solid solid dotted dotted;
+  border-radius: 50%;
+  display: inline-block;
+  position: relative;
+  box-sizing: border-box;
+  animation: rotation 2s linear infinite;
+}
+.Spinner::after {
+  content: '';
+  box-sizing: border-box;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+  border: 10px dotted var(--color-primary);
+  border-style: solid solid dotted;
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  animation: rotationBack 1s linear infinite;
+  transform-origin: center center;
+}
+
+@keyframes rotation {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+@keyframes rotationBack {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(-360deg);
+  }
+}
+</style>
---
+++ src/components/Waiting.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Spinner from './Spinner.vue'
+</script>
+
+<template>
+  <div class="Waiting">
+    <Spinner class="Waiting-spinner" />
+  </div>
+</template>
+
+<style>
+.Waiting {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+</style>
