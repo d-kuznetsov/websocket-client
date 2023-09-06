d05326f997b31f61af2282f6fde0c8df6bf6f4a3
Dmitry Kuznetsov <dmitry.kuznetsov.it@gmail.com>
2023-09-06 08:44:05 +0200

    create web socket service

---
+++ src/services/websocket.js
@@ -0,0 +1,39 @@
+class WebSocketService {
+  constructor() {
+    this.socket = null
+    this.onMessageCallbacks = []
+  }
+
+  connect() {
+    this.socket = new WebSocket(import.meta.env.VITE_WEB_SOCKET)
+
+    this.socket.addEventListener('open', (event) => {
+      console.log('WebSocket connection opened:', event)
+    })
+
+    this.socket.addEventListener('close', (event) => {
+      console.log('WebSocket connection closed:', event)
+    })
+
+    this.socket.addEventListener('message', (event) => {
+      const message = JSON.parse(event.data)
+      this.onMessageCallbacks.forEach((callback) => callback(message))
+    })
+  }
+
+  sendMessage(message) {
+    if (this.socket.readyState === WebSocket.OPEN) {
+      this.socket.send(JSON.stringify(message))
+    }
+  }
+
+  addMessageListener(callback) {
+    this.onMessageCallbacks.push(callback)
+  }
+
+  close() {
+    this.socket.close()
+  }
+}
+
+export default WebSocketService
